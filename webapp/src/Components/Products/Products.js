daa36ee36bfb63a3d8050fa29ae97e9d9149818c
10k20 <10k20.dev@gmail.com>
2021-05-05 09:52:15 +0300

    update product card layout

--- webapp/src/Components/Products/Products.js
+++ webapp/src/Components/Products/Products.js
@@ -10,6 +10,49 @@ const Products = () => {
             <div className="products-wrapper">
                 <Link to={`/products/`}>
                     <div className="product-card">
+                        <div className="product-card-category">
+                            <span>Video</span>
+                        </div>
+                        <div className="product-card-image">
+                            <img src={exampleImage}/>
+                        </div>
+                        <div className="product-card-main">
+                            <div className="product-card-body">
+                                <div className="product-card-body-title">Autumn 2019 LUT Pack</div>
+                                <div className="product-card-body-description">Some description text for this product</div>
+                            </div>
+                            <div className="product-card-footer">
+                                <div className="product-card-author">Ushida</div>
+                                <div className="product-card-price">20$</div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={`/products/`}>
+                    <div className="product-card">
+                        <div className="product-card-category">
+                            <span>Video</span>
+                        </div>
+                        <div className="product-card-image">
+                            <img src={exampleImage}/>
+                        </div>
+                        <div className="product-card-main">
+                            <div className="product-card-body">
+                                <div className="product-card-body-title">Title</div>
+                                <div className="product-card-body-description">Some descafw awf awf awf A fription aw faw af awf af af wfafw a text for this produc afw awfaw faw faw  awf awf awf afawf aw fafw awf awf awft af awfawfawfwaf  afwfawfawfafwafwawffaw fawfawfaw</div>
+                            </div>
+                            <div className="product-card-footer">
+                                <div className="product-card-author">Ushida</div>
+                                <div className="product-card-price">20$</div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={`/products/`}>
+                    <div className="product-card">
+                        <div className="product-card-category">
+                            <span>Video</span>
+                        </div>
                         <div className="product-card-image">
                             <img src={exampleImage}/>
                         </div>
@@ -25,36 +68,46 @@ const Products = () => {
                         </div>
                     </div>
                 </Link>
-                <div className="product-card">
-                    <div className="product-card-image">
-                        <img src={exampleImage}/>
-                    </div>
-                    <div className="product-card-main">
-                        <div className="product-card-body">
-                            <div className="product-card-body-title">Title</div>
-                            <div className="product-card-body-description">Some description text for this product</div>
+                <Link to={`/products/`}>
+                    <div className="product-card">
+                        <div className="product-card-category">
+                            <span>Video</span>
                         </div>
-                        <div className="product-card-footer">
-                            <div className="product-card-author">Ushida</div>
-                            <div className="product-card-price">20$</div>
+                        <div className="product-card-image">
+                            <img src={exampleImage}/>
                         </div>
-                    </div>
-                </div>
-                <div className="product-card">
-                    <div className="product-card-image">
-                        <img src={exampleImage}/>
-                    </div>
-                    <div className="product-card-main">
-                        <div className="product-card-body">
-                            <div className="product-card-body-title">Title</div>
-                            <div className="product-card-body-description">Some description text for this product</div>
-                        </div>
-                        <div className="product-card-footer">
-                            <div className="product-card-author">Ushida</div>
-                            <div className="product-card-price">20$</div>
+                        <div className="product-card-main">
+                            <div className="product-card-body">
+                                <div className="product-card-body-title">Title</div>
+                                <div className="product-card-body-description">Some description text for this product</div>
+                            </div>
+                            <div className="product-card-footer">
+                                <div className="product-card-author">Ushida</div>
+                                <div className="product-card-price">20$</div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Link>
+                <Link to={`/products/`}>
+                    <div className="product-card">
+                        <div className="product-card-category">
+                            <span>Video</span>
+                        </div>
+                        <div className="product-card-image">
+                            <img src={exampleImage}/>
+                        </div>
+                        <div className="product-card-main">
+                            <div className="product-card-body">
+                                <div className="product-card-body-title">Title</div>
+                                <div className="product-card-body-description">Some description text for this product</div>
+                            </div>
+                            <div className="product-card-footer">
+                                <div className="product-card-author">Ushida</div>
+                                <div className="product-card-price">20$</div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
         </div>
     )
