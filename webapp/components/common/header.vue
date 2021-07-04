8f078c26374145ed657bc7e89f5cf8605278a60b
10k20 <10k20.dev@gmail.com>
2021-07-04 17:04:42 +0300

    change react on vue

---
+++ webapp/components/common/header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <div class='header-wrapper'>
+      <div class='header-navigation'>
+        <nuxt-link :to="{name: '/'}" class='header-link'>
+          <span class='header-logo'>Ushida</span>
+        </nuxt-link>
+        <nuxt-link :to="{name: 'products'}" class='header-link link' >
+          <span>Products</span>
+        </nuxt-link>
+        <nuxt-link :to="{name: 'portfolio'}" class='header-link link'>
+          <span>Portfolio</span>
+        </nuxt-link>
+        <nuxt-link :to="{name: 'contacts'}" class='header-link link'>
+          <span>Contacts</span>
+        </nuxt-link>
+        <nuxt-link :to="{name: 'about'}" class='header-link link'>
+          <span>About</span>
+        </nuxt-link>
+      </div>
+      <div class='header-auth'>
+        <button class="button button-not-active sign-in">Sign In</button>
+        <button class="button sign-up">Sign Up</button>
+      </div>
+    </div>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  color: #fff;
+  position: fixed;
+  backdrop-filter: blur(10px);
+  background-color: rgba(0, 0, 0, 0.1);
+  font-size: 0.9rem;
+  z-index: 20;
+  top: 0;
+  width: 100%;
+  .header-wrapper {
+    padding: 0.5rem 1.5rem;
+    display: flex;
+    justify-content: space-between;
+    .header-auth {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .sign-up {
+        margin-left: 0.5rem;
+      }
+    }
+    .header-navigation {
+      display: flex;
+      .header-link {
+        margin-left: 2rem;
+        display: flex;
+        align-items: center;
+        .header-logo {
+          padding-top: 5px;
+          font-family: 'Gang-of-Three', Fallback, sans-serif;
+          font-size: 1.2rem;
+        }
+        span {
+          color: #fff;
+        }
+      }
+      .header-link:first-child {
+        margin-left: 0;
+      }
+    }
+  }
+}
+</style>
---
+++ webapp/layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app">
+    <Header />
+    <main>
+      <Nuxt/>
+    </main>
+    <Footer />
+  </div>
+</template>
+<script>
+import Header from '~/components/common/header.vue'
+import Footer from '~/components/common/footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+#__layout {
+  background-color: rgba(0, 0, 0, 0.85);
+  overflow-x: hidden;
+  width: 100vw;
+  min-height: 100vh;
+  background: url('~/assets/images/background_2.jpg') no-repeat center center fixed;
+  background-size: cover;
+}
+.app {
+  display: flex;
+  min-height: 100vh;
+  width: 100%;
+  flex-direction: column;
+  align-content: space-between;
+  justify-content: space-between;
+}
+main {
+  margin-top: 5rem;
+}
+</style>
